86d2b5e39c87cf0403a4c750c5fda1e929ff58b1
EricoSM <erico_au@hotmail.com>
2020-06-24 10:42:29 -0500

    test1

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index   #<----- direcionamento para as views
+from .views import index   #<----- direcionamento para as views teste
 
 
 urlpatterns = [
     path('', index, name='index'),
 ]
+
